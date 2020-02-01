15e6da5a9cc27ee30b65124b211f12bd39b5d8f9
prav-p <55996583+prav-p@users.noreply.github.com>
2020-02-01 13:21:41 -0800

    Change title

---
+++ build-DialerUI-Desktop_Qt_5_12_6_MinGW_64_bit-Debug/ui_dialerui.h
@@ -0,0 +1,87 @@
+/********************************************************************************
+** Form generated from reading UI file 'dialerui.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.6
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_DIALERUI_H
+#define UI_DIALERUI_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Dialerui
+{
+public:
+    QAction *actionOpen_an_Address_Book;
+    QAction *actionExit;
+    QWidget *centralwidget;
+    QPushButton *pushButton;
+    QMenuBar *menubar;
+    QMenu *menuTools;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *Dialerui)
+    {
+        if (Dialerui->objectName().isEmpty())
+            Dialerui->setObjectName(QString::fromUtf8("Dialerui"));
+        Dialerui->resize(800, 600);
+        actionOpen_an_Address_Book = new QAction(Dialerui);
+        actionOpen_an_Address_Book->setObjectName(QString::fromUtf8("actionOpen_an_Address_Book"));
+        actionExit = new QAction(Dialerui);
+        actionExit->setObjectName(QString::fromUtf8("actionExit"));
+        centralwidget = new QWidget(Dialerui);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(120, 30, 112, 32));
+        Dialerui->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(Dialerui);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 22));
+        menuTools = new QMenu(menubar);
+        menuTools->setObjectName(QString::fromUtf8("menuTools"));
+        Dialerui->setMenuBar(menubar);
+        statusbar = new QStatusBar(Dialerui);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        Dialerui->setStatusBar(statusbar);
+
+        menubar->addAction(menuTools->menuAction());
+        menuTools->addAction(actionOpen_an_Address_Book);
+        menuTools->addSeparator();
+        menuTools->addAction(actionExit);
+
+        retranslateUi(Dialerui);
+
+        QMetaObject::connectSlotsByName(Dialerui);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *Dialerui)
+    {
+        Dialerui->setWindowTitle(QApplication::translate("Dialerui", "Dialerui", nullptr));
+        actionOpen_an_Address_Book->setText(QApplication::translate("Dialerui", "Open an Address Book...", nullptr));
+        actionExit->setText(QApplication::translate("Dialerui", "Exit", nullptr));
+        pushButton->setText(QApplication::translate("Dialerui", "HI", nullptr));
+        menuTools->setTitle(QApplication::translate("Dialerui", "Tools", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Dialerui: public Ui_Dialerui {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_DIALERUI_H
